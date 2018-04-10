bf93335fcba52694511bfa8fb5d278fb95dcdfae
Alexander Hartmaier <abraxxa@cpan.org>
2018-04-10 14:30:50 +0200

    fix new api conformity with Authen::Passphrase
    new takes a list of attribute/value pairs, not a hashref
    this is impracticable as all accessor and OO modules use hashref style but we
    can't change that

--- lib/Authen/Passphrase/Argon2.pm
+++ lib/Authen/Passphrase/Argon2.pm
@@ -5,32 +5,33 @@ package Authen::Passphrase::Argon2;
 
 # ABSTRACT: Argon2 support for Authen::Passphrase
 
-use parent qw( Authen::Passphrase Class::Accessor::Fast );
+use parent 'Authen::Passphrase';
 use Carp;
 use Crypt::Argon2 qw( argon2id_pass argon2id_verify );
 
-__PACKAGE__->mk_accessors(qw( _hash ));
-__PACKAGE__->mk_ro_accessors(
-    qw( salt t_cost m_factor parallelism tag_size
-        passphrase )
-);
-
 sub new {
-    my ( $class, @args ) = @_;
-    my $self = $class->SUPER::new(@args);
+    my $class = shift;
+    my $self = bless( {}, $class );
 
-    croak "passphrase not set"  unless defined $self->passphrase;
-    croak "salt not set"        unless defined $self->salt;
-    croak "t_cost not set"      unless defined $self->t_cost;
-    croak "m_factor not set"    unless defined $self->m_factor;
-    croak "parallelism not set" unless defined $self->parallelism;
-    croak "tag_size not set"    unless defined $self->tag_size;
+    while (@_) {
+        my $attr  = shift;
+        my $value = shift;
+        $self->{$attr} = $value;
+    }
 
-    my $hash = argon2id_pass(
-        $self->passphrase, $self->salt,        $self->t_cost,
-        $self->m_factor,   $self->parallelism, $self->tag_size
-    );
-    $self->_hash($hash);
+    if ( !defined $self->{_hash} ) {
+        croak "passphrase not set"  unless defined $self->{passphrase};
+        croak "salt not set"        unless defined $self->{salt};
+        croak "t_cost not set"      unless defined $self->{t_cost};
+        croak "m_factor not set"    unless defined $self->{m_factor};
+        croak "parallelism not set" unless defined $self->{parallelism};
+        croak "tag_size not set"    unless defined $self->{tag_size};
+
+        my $hash = argon2id_pass(
+            $self->{passphrase}, $self->{salt},        $self->{t_cost},
+            $self->{m_factor},   $self->{parallelism}, $self->{tag_size} );
+        $self->{_hash} = $hash;
+    }
 
     return $self;
 }
@@ -41,12 +42,12 @@ sub from_crypt {
     croak "invalid Argon2 crypt format"
         unless $crypt =~ m/^\$argon2id\$v=\d+\$m=(\d+),t=(\d+),p=(\d+)\$/;
 
-    return $class->SUPER::new( {
+    return $class->new(
         m_factor    => $1,
         t_cost      => $2,
         parallelism => $3,
         _hash       => $crypt,
-    } );
+    );
 }
 
 sub from_rfc2307 {
@@ -59,17 +60,17 @@ sub from_rfc2307 {
 
 sub match {
     my ( $self, $passphrase ) = @_;
-    return argon2id_verify( $self->_hash, $passphrase );
+    return argon2id_verify( $self->{_hash}, $passphrase );
 }
 
 sub as_crypt {
     my $self = shift;
-    return $self->_hash;
+    return $self->{_hash};
 }
 
 sub as_rfc2307 {
     my $self = shift;
-    return '{ARGON2}' . $self->_hash;
+    return '{ARGON2}' . $self->{_hash};
 }
 
 =head1 SYNOPSIS
@@ -77,14 +78,14 @@ sub as_rfc2307 {
 use Authen::Passphrase::Argus2;
 
 # hash a password
-my $ppr = Authen::Passphrase::Argon2->new({
+my $ppr = Authen::Passphrase::Argon2->new(
     passphrase  => 'password1',
     salt        => 'longenoughsalt',
     t_cost      => 16,
     m_factor    => "4M",
     parallelism => 1,
     tag_size    => 16,
-});
+);
 
 my $crypt = $ppr->as_crypt;
 my $rfc2307 = $ppr->as_rfc2307;
--- t/argon2.t
+++ t/argon2.t
@@ -4,14 +4,14 @@ use warnings;
 use Test::More;
 BEGIN { use_ok('Authen::Passphrase::Argon2') }
 
-my $x = Authen::Passphrase::Argon2->new( {
+my $x = Authen::Passphrase::Argon2->new(
     passphrase  => 'password1',
     salt        => 'longenoughsalt',
     t_cost      => 16,
     m_factor    => "4M",
     parallelism => 1,
     tag_size    => 16,
-} );
+);
 
 ok $x->match('password1'), 'match with correct password ok';
 ok !$x->match('password2'), 'match with incorrect password fails';
@@ -28,9 +28,9 @@ subtest 'from_crypt' => sub {
         '$argon2id$v=19$m=4096,t=16,p=1$bG9uZ2Vub3VnaHNhbHQ$5zFJ2PN5Lsk1GLvB3FrSyg'
     ), 'from_crypt';
 
-    is $y->m_factor,    4096, 'm_factor ok';
-    is $y->t_cost,      16,   't_cost ok';
-    is $y->parallelism, 1,    'parallelism ok';
+    is $y->{m_factor},    4096, 'm_factor ok';
+    is $y->{t_cost},      16,   't_cost ok';
+    is $y->{parallelism}, 1,    'parallelism ok';
 
     ok $y->match('password1'), 'match with correct password ok';
     ok !$y->match('password2'), 'match with incorrect password fails';
@@ -41,9 +41,9 @@ subtest 'from_rfc2307' => sub {
         '{ARGON2}$argon2id$v=19$m=4096,t=16,p=1$bG9uZ2Vub3VnaHNhbHQ$5zFJ2PN5Lsk1GLvB3FrSyg'
     ), 'from_rfc2307';
 
-    is $y->m_factor,    4096, 'm_factor ok';
-    is $y->t_cost,      16,   't_cost ok';
-    is $y->parallelism, 1,    'parallelism ok';
+    is $y->{m_factor},    4096, 'm_factor ok';
+    is $y->{t_cost},      16,   't_cost ok';
+    is $y->{parallelism}, 1,    'parallelism ok';
 
     ok $y->match('password1'), 'match with correct password ok';
     ok !$y->match('password2'), 'match with incorrect password fails';
